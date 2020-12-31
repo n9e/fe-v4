3100bdc5e8dc98703f0d2348440130b55315c94b
liujingjingg <949694044@qq.com>
2020-12-31 16:22:56 +0800

    fix: add 401 & 403 status

--- rdb-web/src/pages/ResourcesTree/Usages/request.tsx
+++ rdb-web/src/pages/ResourcesTree/Usages/request.tsx
@@ -20,6 +20,13 @@ export const getUsages = async (tenant: number) => {
     })
     const data = await response.json()
     if(response.status < 200 || response.status >= 300){
+      if (response.status === 401) {
+        const redirect = window.location.pathname;
+        window.location.href = `/login?redirect=${redirect}`;
+      }
+      if (response.status === 403) {
+        window.location.href = '/403';
+      }
       console.log(data.err);
     }
     return data;
